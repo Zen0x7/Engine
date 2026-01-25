c1402d0ec92d3c874ab645b315bba5bd181a742a
Ian Torres <iantorres@outlook.com>
2026-01-25 06:40:51 -0300

    Send wip.

--- tests/engine/parser.cc
+++ tests/engine/parser.cc
@@ -470,3 +470,82 @@ TEST(parser_test, it_can_parse_broadcast) {
     ASSERT_EQ(_result[0].get_parameters()[1][4], std::byte{ 0x6F });
 }
 
+
+TEST(parser_test, it_can_parse_send) {
+    std::array _stream = {
+        std::byte{ 0x01 }, // Number of items (ie: 1)
+
+        // Length of entry
+        std::byte{ 0x00 },
+        std::byte{ 0x1B },
+
+        // Entry type (1 byte, ie: 8 = SEND)
+        std::byte{ 0x08 },
+
+        // Entry parameters (1 byte, ie: 1)
+        std::byte{ 0x03 },
+
+        // 1° parameter length (2 bytes, ie: 16)
+        std::byte{ 0x00 },
+        std::byte{ 0x10 },
+
+        // 1° parameter value (16 bytes ... is a uuid v4)
+        std::byte{ 0xA2 },
+        std::byte{ 0xCF },
+        std::byte{ 0xAA },
+        std::byte{ 0xCF },
+        std::byte{ 0x6E },
+        std::byte{ 0xBC },
+        std::byte{ 0x4F },
+        std::byte{ 0x18 },
+        std::byte{ 0x91 },
+        std::byte{ 0xDF },
+        std::byte{ 0x20 },
+        std::byte{ 0x80 },
+        std::byte{ 0x6F },
+        std::byte{ 0x0B },
+        std::byte{ 0x54 },
+        std::byte{ 0x2A },
+
+        // 2° parameter length (2 bytes, ie: 5)
+        std::byte{ 0x00 },
+        std::byte{ 0x05 },
+
+        // 2° parameter value (5 bytes ... is "hello")
+        std::byte{ 0x68 },
+        std::byte{ 0x65 },
+        std::byte{ 0x6C },
+        std::byte{ 0x6C },
+        std::byte{ 0x6F },
+    };
+
+    auto _result = parse(_stream);
+
+    ASSERT_EQ(_result.size(), 1);
+    ASSERT_EQ(_result[0].get_action(), SEND);
+    ASSERT_EQ(_result[0].get_parameters().size(), 2);
+    ASSERT_EQ(_result[0].get_parameters()[0].size(), 16);
+    ASSERT_EQ(_result[0].get_parameters()[0][0], std::byte{ 0xA2 });
+    ASSERT_EQ(_result[0].get_parameters()[0][1], std::byte{ 0xCF });
+    ASSERT_EQ(_result[0].get_parameters()[0][2], std::byte{ 0xAA });
+    ASSERT_EQ(_result[0].get_parameters()[0][3], std::byte{ 0xCF });
+    ASSERT_EQ(_result[0].get_parameters()[0][4], std::byte{ 0x6E });
+    ASSERT_EQ(_result[0].get_parameters()[0][5], std::byte{ 0xBC });
+    ASSERT_EQ(_result[0].get_parameters()[0][6], std::byte{ 0x4F });
+    ASSERT_EQ(_result[0].get_parameters()[0][7], std::byte{ 0x18 });
+    ASSERT_EQ(_result[0].get_parameters()[0][8], std::byte{ 0x91 });
+    ASSERT_EQ(_result[0].get_parameters()[0][9], std::byte{ 0xDF });
+    ASSERT_EQ(_result[0].get_parameters()[0][10], std::byte{ 0x20 });
+    ASSERT_EQ(_result[0].get_parameters()[0][11], std::byte{ 0x80 });
+    ASSERT_EQ(_result[0].get_parameters()[0][12], std::byte{ 0x6F });
+    ASSERT_EQ(_result[0].get_parameters()[0][13], std::byte{ 0x0B });
+    ASSERT_EQ(_result[0].get_parameters()[0][14], std::byte{ 0x54 });
+    ASSERT_EQ(_result[0].get_parameters()[0][15], std::byte{ 0x2A });
+    ASSERT_EQ(_result[0].get_parameters()[1].size(), 5);
+    ASSERT_EQ(_result[0].get_parameters()[1][0], std::byte{ 0x68 });
+    ASSERT_EQ(_result[0].get_parameters()[1][1], std::byte{ 0x65 });
+    ASSERT_EQ(_result[0].get_parameters()[1][2], std::byte{ 0x6C });
+    ASSERT_EQ(_result[0].get_parameters()[1][3], std::byte{ 0x6C });
+    ASSERT_EQ(_result[0].get_parameters()[1][4], std::byte{ 0x6F });
+}
+
